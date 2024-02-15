12df1a64239809d8b5ae22a752acb413d9bef258
Nicola Calvio <info@nicolacalvio.com>
2024-02-15 15:24:31 +0100

    aggiunto file per check di soddisfacibilità

---
+++ check_satistable_preposition.py
@@ -0,0 +1,12 @@
+from sympy import symbols, Or, Implies, satisfiable
+
+# Definizione delle variabili proposizionali
+A, B = symbols('A B')
+
+# Definizione dell'espressione A OR B → B OR A
+expression = Implies(Or(A, B), Or(B, A))
+
+# Verifica della soddisfacibilità
+result = satisfiable(expression)
+
+print(result)
