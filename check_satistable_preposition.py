57acc5cef6a4f56cdef31ffc6236d2c4a157362a
Nicola Calvio <info@nicolacalvio.com>
2024-02-16 18:36:54 +0100

    correzioni

--- check_satistable_preposition.py
+++ check_satistable_preposition.py
@@ -1,11 +1,10 @@
-from sympy import symbols, Or, Implies, satisfiable
+from sympy import symbols, Or, Implies, satisfiable, Not
 
 # Definizione delle variabili proposizionali
 A, B = symbols('A B')
 
 # Definizione dell'espressione A OR B → B OR A
 expression = Implies(Or(A, B), Or(B, A))
-
 # Verifica della soddisfacibilità
 result = satisfiable(expression)
 
